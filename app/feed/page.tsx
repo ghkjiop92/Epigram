71255fdc3ae54b3cbfa9f3f2815934b448338143
강동우 <gangdong-u@gangdong-uui-MacBookAir.local>
2025-03-27 13:20:49 +0900

    feat:네비게이션 바 구현

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -2,7 +2,7 @@ export default function FeedPage() {
     return (
       <div>
         <h1>피드 페이지</h1>
+        <p>이곳에 피드 내용을 작성합니다.</p>
       </div>
     );
   }
-  
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,48 +1,36 @@
 import Link from "next/link";
 import Image from "next/image";
-import styles from "./Navbar.module.scss"
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav
-      className="h-80 w-[1920px] py-5" 
-      style={{ backgroundColor: "#ffffff" }}
-    >
-      <div 
-    className={styles.test}
-        style={{
-          marginLeft: 120, 
-          marginRight: 120, 
-        }}
-      >
-        <Link href="/">
-        <Image 
-          src="/images/Book.png" 
-          alt="Book logo" 
-          width={23.04}  
-          height={23.04} 
-          style={{ marginRight: 20 }}
-         
-          priority
-        />
-
+    <>
+      <nav className={styles.navcolor}>
+        <Link href={"/"}>
+          <Image 
+            src="/images/Book.png" 
+            alt="Book logo" 
+            width={36} 
+            height={36}
+            className={styles["book-logo"]}
+            priority
+          />
         </Link>
-    
-        <Link href="/">
-        <Image 
-          src="/images/Epigram.png" 
-          alt="Epigram logo" 
-          width={91}  
-          height={26} 
-          priority
-        />
-            </Link>
-
-    <Link  href={"/feed"} className="ml-{36px}  mt-5 h-[28px] w-[26px]  text-[#373737]"
-    style={{ textDecoration: "none" ,marginRight:36 }}>
+        <Link href={"/"}>
+          <Image 
+            src="/images/Epigram.png" 
+            alt="Epigram logo" 
+            width={91} 
+            height={26}
+            className={styles["Epigram-logo"]}
+            priority
+          />
+        </Link>
+        <Link href="/feed" className={styles.feed}>
           피드
         </Link>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
+
