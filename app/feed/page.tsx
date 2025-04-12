7e5a281657dc0e5aea46f6c22666203d6b514a9b
Epigram <ghkjiop92@naver.com>
2025-04-12 13:12:44 +0900

    git commit -m "feat:에픽그램 리스트 작업페이지"

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export default function FeedPage() {
-    return (
-      <div>
-        <h1>피드 페이지</h1>
-        <p>이곳에 피드 내용을 작성합니다.</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>피드 페이지</h1>
+      <p>이곳에 피드 내용을 작성합니다.</p>
+    </div>
+  );
+}
